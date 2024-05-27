7915a09fbec52182d1fe7337e41b4b1869f9aa66
Brodich Vlad <brodich_vlad@ukr.net>
2024-05-27 14:26:50 +0300

    fix style

--- src/components/PaymentModal/FormPayment/FormPayment.jsx
+++ src/components/PaymentModal/FormPayment/FormPayment.jsx
@@ -92,7 +92,7 @@ export default function FormPayment({handleThank}) {
         className={styles.input}
         ref={inputRef}
         size={ amount.length < 1 ? 1 : amount.length } 
-        maxLength={amount.length < 1 ? 2 : amount.length+1 }
+        maxLength={amount.length < 1 ? 1 : amount.length+1 }
         onInput={(e)=>{isValidate(e.target.value)}}
         onBlur={(e)=>{fixedAmount(e)}}
         readOnly={readOnly}
