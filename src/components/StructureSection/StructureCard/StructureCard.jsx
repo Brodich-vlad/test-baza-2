366c642ba4c5c05bd52f84ae3bf101d9162890cf
Brodich Vlad <brodich_vlad@ukr.net>
2024-06-02 12:54:00 +0300

    fix style HeroCard

--- src/components/StructureSection/StructureCard/StructureCard.jsx
+++ src/components/StructureSection/StructureCard/StructureCard.jsx
@@ -1,5 +1,6 @@
 import styles from "./StructureCard.module.scss";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function StructureCard({ item }) {
@@ -9,14 +10,26 @@ export default function StructureCard({ item }) {
   return (
     <div className={styles.cardStyle}>
       <div className={styles.cardWrapper}>
-        <div className={styles.ellipseWrapper}></div>
-        <img
+        {/* <div className={styles.ellipseWrapper}></div> */}
+        {/* <img
           className={styles.svgWrapper}
           src={img}
           width={width}
           height={height}
           alt={t(title)}
-        />
+        /> */}
+        <div className={styles.imgWrapper} style={{width:`${width}`,height:`${height}`}}>
+            <Image
+              className={styles.image}
+              src={img}
+              fill
+              sizes="100%"
+              alt={t(title)}
+              quality={80}
+            />
+          {/* <div className={styles.ellipseWrapper}></div>   */}
+        </div>
+
       </div>
       <div className={styles.textCard}>
         <Link
