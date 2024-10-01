43e566a7575a1f9a7350a2d8d8d0c787658133bc
Brodich Vlad <brodich_vlad@ukr.net>
2024-10-01 13:13:25 +0300

    update project

--- src/components/modals/PaymentModal/PaymentModal.jsx
+++ src/components/modals/PaymentModal/PaymentModal.jsx
@@ -1,8 +1,7 @@
 "use client";
 import styles from './PaymentModal.module.scss';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useTranslations } from "next-intl";
-import { isMobile } from 'react-device-detect';
 import { useParams } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { PaymentService } from '@/src/api/payment';
@@ -13,50 +12,37 @@ import FormPayment from './FormPayment/FormPayment';
 import MessageCard from './MessageCard/MessageCard';
 import CloseBtn from '../../shared/CloseBtn/CloseBtn';
 import Loader from '../../shared/loader/Loader';
-import { usePathname } from '@/src/navigation';
 
 export default function PaymentModal() {
   // Мова сторінки.
   const { locale } = useParams();
-  const pathname = usePathname()
+
+  // контент.
+  const t = useTranslations("Modal_support");
 
   // Отримуємо стан.
   const isOpen = stateModalPayment(state => state.isOpen);
   const close = stateModalPayment(state => state.close);
-  const [ processing, setProcessing ]= useState(false)
-  const [ isThanks, setIsThanks ]= useState(false)
-  const [ pathName, setPathName ]= useState(pathname)
+  const [ processing, setProcessing ] = useState(false);
+  const [ isThanks, setIsThanks ] = useState(false);
 
-  // function thanks() {
-  //   if(isMobile){
-  //     setProcessing(true)
-  //     setTimeout(()=>{
-  //       setProcessing(false)
-  //       setIsThanks(true)
-  //     },3000)
-  //   }
-  // }
+  // Вікно подяки через 3 секунди.
+  function thanks() {
+    setProcessing(true)
+    setTimeout(()=>{
+      setProcessing(false)
+      setIsThanks(true)
+    },3000)
+  }
 
+  // Запит отримання посилання на платіжну сторінку.
   const { mutate, isPending, isError, isSuccess, reset } = useMutation({
     mutationFn: (amount, locale) => {
       return PaymentService(amount, locale)
     },onSuccess:()=>{
-      //thanks()
+      thanks()
     }
   })
-  useEffect(()=>{
-    if(isMobile){
-      if(isSuccess){
-        setProcessing(true)
-        if(pathName !== pathname){
-          setProcessing(false)
-          setIsThanks(true)
-        }
-      }
-    }
-  },[isMobile, pathname, isSuccess])
-  // контент.
-  const t = useTranslations("Modal_support");
 
   const handleClose = useCallback(() => {
     setProcessing(false)
@@ -73,34 +59,32 @@ export default function PaymentModal() {
     if(isError){
       return <MessageCard handleClose={handleClose} isError={isError}/>
     }else if(isSuccess){
-      if(isMobile){
-        if(processing){
-          return <FormPayment handleSubmit={handleSubmit}/>
-        }
-        if(isThanks){
-          return <MessageCard handleClose={handleClose} isThanks={isThanks}/>
-        }
-      }else return <MessageCard handleClose={handleClose} isThanks={isSuccess}/>
+      if(processing){
+        return <FormPayment handleSubmit={handleSubmit}/>
+      }
+      if(isThanks){
+        return <MessageCard handleClose={handleClose} isThanks={isThanks}/>
+      }
     }
     else return <FormPayment handleSubmit={handleSubmit}/>
   }
 
+  return (
+    <LayoutModal isOpen={isOpen} handleClose={handleClose}>
+      <div className={styles.wrapper}>
+        <div className={styles.card}>
+          {isPending && <Loader/>}
+          {processing && <Loader/>}
+          { renderContent() }
 
-
-  return <LayoutModal isOpen={isOpen} handleClose={handleClose}>
-    <div className={styles.wrapper}>
-      <div className={styles.card}>
-        {isPending && <Loader/>}
-        {processing && <Loader/>}
-        { renderContent() }
-
-        <CloseBtn className={styles.close_btn}
-          ariaLabel={ isSuccess ? 
-            t('ariaLabel_btn_close_2') : 
-            t('ariaLabel_btn_close_1')
-          }
-          onClick={handleClose}/>
+          <CloseBtn className={styles.close_btn}
+            ariaLabel={ isSuccess ? 
+              t('ariaLabel_btn_close_2') : 
+              t('ariaLabel_btn_close_1')
+            }
+            onClick={handleClose}/>
+        </div>
       </div>
-    </div>
-  </LayoutModal>
+    </LayoutModal>
+  )
 }
